6ac8a00c803155bf67c8362cee1f05b990552838
Vladyslav Shaposhnikov <vladyslav.shaposhnik@microsoft.wsei.edu.pl>
2024-10-15 17:25:52 +0200

    Add CalculatorController.cs and Index.cshtml

---
+++ WebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class CalculatorController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Result(string op, double x, double y)
+    {
+        ViewBag.result_x = x;
+        ViewBag.result_y = y;
+        
+        ViewBag.result_op = op;
+        
+        switch(op)
+        {
+            case "add":
+                ViewBag.Result = x + y;
+                break;
+            case "sub":
+                ViewBag.Result = x - y;
+                break;
+            case "mul":
+                ViewBag.Result = x * y;
+                break;
+            case "div":
+                ViewBag.Result = x / y;
+                break;
+        }
+        return View();
+    }
+}
